db93b22bd2402a75fed707ef7cfbfdf0f4865612
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-08-14 09:56:06 +0700

    update entity

--- src/main/java/com/example/recruitmentweb/jobportal/service/AuthService.java
+++ src/main/java/com/example/recruitmentweb/jobportal/service/AuthService.java
@@ -5,5 +5,5 @@ import com.example.recruitmentweb.jobportal.domain.payload.request.SignUpRequest
 import java.net.URI;
 
 public interface AuthService {
-    URI registerAccount(SignUpRequest request)
+    URI registerAccount(SignUpRequest request);
 }
