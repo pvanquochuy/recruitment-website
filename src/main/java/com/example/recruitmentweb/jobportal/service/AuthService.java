8a1ad2810074603b5316ed8b44f88126d33ba502
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-08-14 09:55:21 +0700

    update entity

--- src/main/java/com/example/recruitmentweb/jobportal/service/AuthService.java
+++ src/main/java/com/example/recruitmentweb/jobportal/service/AuthService.java
@@ -1,5 +1,7 @@
 package com.example.recruitmentweb.jobportal.service;
 
+import com.example.recruitmentweb.jobportal.domain.payload.request.SignUpRequest;
+
 import java.net.URI;
 
 public interface AuthService {
